bee3decf327c36ad2f13c3ddb1d03dfe579f6aa5
Mohd Wasif Raza <wasraz86@gmail.com>
2023-11-14 17:48:53 +0530

    Initial Commit

---
+++ Profile View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Profile View
+//
+//  Created by Mohd Wasif Raza on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Profile View/Profile_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_ViewApp.swift
+//  Profile View
+//
+//  Created by Mohd Wasif Raza on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Profile_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
